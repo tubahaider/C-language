1fefd6ea06c8389bd5e9dc3421f110a52649c4ad
Tuba Haider <tubahaider2006@gmail.com>
2025-06-23 15:27:07 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,53 @@
+
+
+#include<stdio.h>
+int main (){
+  printf("Hello world ");
+  return 0;
+}
+
+/*
+#include<stdio.h>
+int main (){
+  int age = 56;
+  printf("Age is %d",age);
+  return 0;
+
+}
+
+ //Case-Real NUmber
+ #include<stdio.h>
+ int main (){
+  float pi = 3.14;
+  printf("Value of pi is %f",pi);
+  return 0;
+
+ }
+  
+
+
+ #include<stdio.h>
+ int main(){
+  char star= '*';
+  printf("Star look like this: %c",star);
+  return 0;
+
+
+ }
+  
+ //Input- Scanf()
+
+ #include<stdio.h>
+ int main(){
+  int age;
+  printf("Enter age = ");
+  scanf("%d", &age);
+  printf("Age is %d", age);
+  return 0;
+
+ }
+ 
+*/
+
+
+
