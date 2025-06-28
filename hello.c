796febbea13d0c9d3b541c6fd2c92dae95d0cf26
Tuba Haider <tubahaider2006@gmail.com>
2025-06-28 23:13:13 +0530

    The first commit

--- hello.c
+++ hello.c
@@ -82,3 +82,21 @@ int main(){
   return 0;
   
 }
+//if,else-if,else
+#include<stdio.h>
+int main (){
+  int age;
+  printf("enter age:");
+  scanf("%d", &age);
+
+  if (age>=18){
+    printf("Adult\n");
+  }
+  else if (age >13 && age <18){
+    printf("Teenager\n");
+  }
+  else {
+    printf("Not adult\n");
+
+  }
+}
