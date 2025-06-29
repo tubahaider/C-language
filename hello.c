a404c9bc2ccb7a179735e12a187897b9250493d5
Tuba Haider <tubahaider2006@gmail.com>
2025-06-29 22:43:04 +0530

    My first commit

--- hello.c
+++ hello.c
@@ -101,7 +101,7 @@ int main (){
 
   }
 }
-  */
+ 
  //Ternary 
  #include<stdio.h>
  int main(){
@@ -111,3 +111,34 @@ int main (){
   age>= 18? printf("adult\n"):printf("not adult \n");
   return 0;
  }
+  */
+ //Swith formet
+ #include<stdio.h>
+ int main(){
+   int day ;
+   printf("enter day (1-7):");
+   scanf ("%d", &day);
+
+   switch (day){
+   case 1 : printf("monday\n");
+   break;
+   case 2 : printf("tuesday\n");
+   break;
+   case 3 : printf("wednesday\n");
+   break;
+   case 4 : printf ("thrsday \n");
+   break;
+   case 5 : printf("friday\n");
+   break;
+   case 6 : printf("saturday\n");
+   break;
+   case 7 : printf("sunday \n");
+   break;
+   
+   default: printf("not a valid day !\n");
+   
+   }
+   return 0;
+ 
+  }
+
